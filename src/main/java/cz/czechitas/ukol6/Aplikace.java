0451387e5c00d06618f7d478237240319f7a60fc
PavlinaRacakova <pavlina.kupkova@email.cz>
2023-04-13 21:45:43 +0200

    The farm

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -6,16 +6,32 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Aplikace extends JFrame {
+
+    private JSpinner geeseSpinner;
+    private JSpinner rabbitsSpinner;
+    private JTextField legsField;
+    private JTextField headsField;
+    private JLabel geeseLabel;
+    private JLabel rabbitsLabel;
+    private JLabel legsLabel;
+    private JLabel headsLabel;
+    private JButton calculateButton;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
         new Aplikace().start();
     }
 
+    /**
+     * Constructor
+     *
+     * @throws HeadlessException
+     */
     public Aplikace() throws HeadlessException {
-        super("Farmářka 1.0");
+        super("The farm");
         this.init();
     }
 
@@ -24,15 +40,72 @@ public class Aplikace extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Initialization code
+     */
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setIconImage(new ImageIcon(Aplikace.class.getResource("czechitas-icon.png")).getImage());
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
         setMinimumSize(new Dimension(250, 200));
 
-        //TODO implementovat formulář podle zadání
+        //Geese spinner
+        SpinnerNumberModel geeseSpinnerNumberModel = new SpinnerNumberModel(0, 0, 10000, 5);
+        geeseSpinner = new JSpinner(geeseSpinnerNumberModel);
+        geeseLabel = new JLabel("Geese ");
+        geeseLabel.setLabelFor(geeseSpinner);
+        add(geeseLabel);
+        add(geeseSpinner);
+
+        //Rabbits spinner
+        SpinnerNumberModel rabbitsSpinnerNumberModel = new SpinnerNumberModel(0, 0, 10000, 5);
+        rabbitsSpinner = new JSpinner(rabbitsSpinnerNumberModel);
+        rabbitsLabel = new JLabel("Rabbits ");
+        rabbitsLabel.setLabelFor(rabbitsSpinner);
+        add(rabbitsLabel);
+        add(rabbitsSpinner);
+
+        //Calculate button
+        calculateButton = new JButton("Calculate");
+        JPanel calculateButtonPanel = new JPanel();
+        calculateButtonPanel.add(calculateButton);
+        add(calculateButtonPanel, "span");
+
+        //Heads field
+        headsField = new JTextField();
+        headsLabel = new JLabel("Number of heads: ");
+        headsLabel.setLabelFor(headsField);
+        headsField.setEditable(false);
+        headsField.setHorizontalAlignment(JTextField.TRAILING);
+        add(headsLabel);
+        add(headsField);
+
+        //Legs field
+        legsField = new JTextField();
+        legsLabel = new JLabel("Number of legs: ");
+        legsLabel.setLabelFor(legsField);
+        legsField.setEditable(false);
+        legsField.setHorizontalAlignment(JTextField.TRAILING);
+        add(legsLabel);
+        add(legsField);
 
         pack();
+
+        calculateButton.addActionListener(this::handleCalculate);
     }
 
+    /**
+     * When the calculate button is invoked, this method counts how many heads and legs are presented and sets the results to relevant fields
+     * @param actionEvent
+     */
+    private void handleCalculate(ActionEvent actionEvent) {
+         int numberOfGeese = (Integer) geeseSpinner.getValue();
+         int numberOfRabbits = (Integer) rabbitsSpinner.getValue();
+
+         int numberOfHeads = numberOfGeese + numberOfRabbits;
+         int numberOfLegs = (numberOfGeese * 2) + (numberOfRabbits * 4);
+
+         headsField.setText(Integer.toString(numberOfHeads));
+         legsField.setText(Integer.toString(numberOfLegs));
+    }
 }
